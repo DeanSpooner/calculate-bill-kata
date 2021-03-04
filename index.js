5b1bb2154917215b4209b80662e27e063e75d177
Dean Spooner <dean.spooner@me.com>
2021-03-04 21:40:52 +0000

    Passed all the tests with this function.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function calculateBill() {
+const calculateBill = function (price, vat, tip) {
 
+    if (!price || !vat || !tip) {
+        return "error";
+    } else {
+        let total = price + vat + tip;
+        return "£" + total;
+    };
 };
 
 module.exports = calculateBill;
